948c4b9aa273aa55e4c64d8712a59feff9b5b11c
Thilanga Pitigala <pittado@gmail.com>
2019-05-15 01:13:57 +1000

    Updated to add data stream support

--- src/AccountRightRequest.php
+++ src/AccountRightRequest.php
@@ -2,11 +2,12 @@
 
 namespace Tenfef\MYOB;
 
+use GuzzleHttp\Psr7\Response;
 use UnexpectedValueException;
 
 class AccountRightRequest
 {
-    function __construct($provider, $token, $username = null, $password = null)
+    public function __construct($provider, $token, $username = null, $password = null)
     {
         $this->token = $token;
         $this->provider = $provider;
@@ -14,7 +15,7 @@ class AccountRightRequest
         $this->password = $password;
     }
 
-    function fetchWithPagination($uri)
+    public function fetchWithPagination($uri): array
     {
         $result = $this->fetch($uri);
         if (!isset($result['Items'])) {
@@ -30,7 +31,7 @@ class AccountRightRequest
         return $items;
     }
 
-    function fetch($uri)
+    public function fetch($uri): array
     {
         $options = ['headers' => $this->provider->getHeaders($this->token, $this->username, $this->password)];
         $request = $this->provider->getRequest(Provider::METHOD_GET, $uri, $options);
@@ -45,26 +46,32 @@ class AccountRightRequest
         return $this->provider->getParsedResponse($request);
     }
 
-    function post($URI, $data)
+    public function fetchStream($uri): Response
+    {
+        $options = ['headers' => $this->provider->getHeaders($this->token, $this->username, $this->password)];
+        $request = $this->provider->getRequest(Provider::METHOD_GET, $uri, $options);
+
+        return $this->provider->getResponse($request);
+    }
+
+    public function post($URI, $data)
     {
         return $this->provider->post("/accountright/" . $URI, $data, $this->token, $this->username, $this->password);
     }
 
-    function put($URI, $data)
+    public function put($URI, $data)
     {
         return $this->provider->put("/accountright/" . $URI, $data, $this->token, $this->username, $this->password);
     }
 
-    function delete($URI)
+    public function delete($URI)
     {
         return $this->provider->delete("/accountright/" . $URI, $this->token, $this->username, $this->password);
     }
 
-    function postFullResponse($URI, $data)
+    public function postFullResponse($URI, $data)
     {
         return $this->provider->postFullResponse("/accountright/" . $URI, $data, $this->token, $this->username,
             $this->password);
     }
-
-
 }
